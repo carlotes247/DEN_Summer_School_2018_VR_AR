fb904d9d8d33060c0a8eb39fb48a7764e5a19e8a
Rob Homewood <rob.homewood@googlemail.com>
2018-06-29 20:32:00 +0100

    initial project commit

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+    public GameObject Bullet;
+	public Transform Gun;
+	public float BulletForce;
+
+	void Start()
+	{
+//		FireBullet();
+	}
+	
+	void FireBullet()
+	{
+		Gun = Camera.main.transform;
+		GameObject newBullet = Instantiate(Bullet, Gun.position + Gun.transform.forward, Gun.rotation);
+		newBullet.GetComponent<Rigidbody>().AddForce(newBullet.transform.forward * BulletForce, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+		{
+			FireBullet();
+		}
+	}
+}

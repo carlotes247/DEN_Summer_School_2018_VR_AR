7d6c8a87eb93c2d73ab371518cb30cff798edc8a
Carlotes_Laptop <carlotes247@gmail.com>
2018-07-02 18:08:02 +0100

    Example scene 03 gun

--- Assets/Examples/Scripts/BulletController_Example.cs
+++ Assets/Examples/Scripts/BulletController_Example.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class BulletController_Example : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public float TimeToLive;
+    private float m_Timer;
+
+    // Use this for initialization
+    void Start () {
+        // Start the internal timer with the time to live
+        m_Timer = TimeToLive;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        // If timer is depleted...
+        if (m_Timer <= 0)
+        {
+            // We reset the timer
+            m_Timer = TimeToLive;
+            // And destroy the bullet
+            Destroy(this.gameObject);
+        }
+        // If it is not...
+        else
+        {
+            // Run timer down
+            m_Timer -= Time.deltaTime;
+        }
+
+
+    }
 }

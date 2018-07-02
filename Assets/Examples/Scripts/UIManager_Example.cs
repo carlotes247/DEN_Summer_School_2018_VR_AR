96d9b1160a404964dba967272336b44f1d0cd7e7
Carlotes_Laptop <carlotes247@gmail.com>
2018-07-02 19:23:06 +0100

    Added all example scenes

--- Assets/Examples/Scripts/UIManager_Example.cs
+++ Assets/Examples/Scripts/UIManager_Example.cs
@@ -4,15 +4,34 @@ using UnityEngine.UI;
 using UnityEngine;
 
 public class UIManager_Example : MonoBehaviour {
-	
-	public Text ScoreText;
+
+    /// <summary>
+    /// Reference to the score Text object in the scene
+    /// </summary>
+    public Text ScoreText;
+    /// <summary>
+    /// Reference to the health Text object in the scene
+    /// </summary>
 	public Text HealthText;
+    /// <summary>
+    /// Reference to the Game Over Text onject in the scene
+    /// </summary>
 	public GameObject GameOverText;
+    /// <summary>
+    /// Private variable containing the game score
+    /// </summary>
 	private int _gameScore = 0;
+    /// <summary>
+    /// Reference to the player component in the scene
+    /// </summary>
+    private PlayerController_Example m_Player;
 
 	// Use this for initialization
 	private void Start () {
-		UpdateUI();
+        // Find the player reference when the game starts
+        m_Player = FindObjectOfType<PlayerController_Example>();
+        // Update UI to whatever values we have at start
+        UpdateUI();
 	}
 	
 	// Update is called once per frame
@@ -20,17 +39,34 @@ public class UIManager_Example : MonoBehaviour {
 		
 	}
 
+    /// <summary>
+    /// Method to end the game
+    /// </summary>
 	public void EndGame()
 	{
 		GameOverText.SetActive(true);
 	}
 
+    /// <summary>
+    /// Method to update the User Interface (UI)
+    /// </summary>
 	public void UpdateUI()
 	{
-		ScoreText.text = "Score: " + _gameScore;
-		HealthText.text = "Health: " + FindObjectOfType<PlayerController_Example>().PlayerHealth;
-	}
+        // If we have the references to our objects...
+        if (ScoreText != null)
+        {
+            ScoreText.text = "Score: " + _gameScore;
+        }
+        if (m_Player != null)
+        {
+            HealthText.text = "Health: " + FindObjectOfType<PlayerController_Example>().PlayerHealth;
+        }
+    }
 
+    /// <summary>
+    /// Method to add score
+    /// </summary>
+    /// <param name="_newScore"></param>
 	public void AddScore(int _newScore)
 	{
 		_gameScore += _newScore;

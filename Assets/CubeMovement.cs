999fbd2e9309d6e38a3e7d40b4243b0beb5094e3
Carlotes_Laptop <carlotes247@gmail.com>
2018-07-03 14:30:50 +0100

    created workshop scene Hello world scene

---
+++ Assets/CubeMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void MoveCube()
+    {
+        transform.Translate(transform.forward * 0.1f);
+    }
+
+    void RotateCube()
+    {
+        transform.Rotate(transform.up * 1.1f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        RotateCube();	
+	}
+}
---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+    public string StartText;
+    public string UpdateText;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(StartText);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(UpdateText);
+	}
+}

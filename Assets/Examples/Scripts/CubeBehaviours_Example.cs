0f67c6dd4af01eb8d85fd093efbd7101bf4d04b5
Carlotes_Laptop <carlotes247@gmail.com>
2018-07-03 10:02:56 +0100

    Simplified cube behaviours

--- Assets/Examples/Scripts/CubeBehaviours_Example.cs
+++ Assets/Examples/Scripts/CubeBehaviours_Example.cs
@@ -14,27 +14,15 @@ public class CubeBehaviours_Example : MonoBehaviour
 	/// <summary>
 	/// Private variable to hold the current direction of the movement
 	/// </summary>
-	private float _direction = 1;
+	public float Direction = 1;
 
 	/// <summary>
 	/// Function to rotate the cube
 	/// </summary>
 	void RotateCube()
 	{
-		// If the rotation is above the threshold
-		if (transform.rotation.y > 150.0f)
-		{
-			// Switch direction
-			_direction = -1;
-		}
-		// If the rotation is below the threshold
-		else if (transform.rotation.y < -150.0f)
-		{
-			// Switch direction
-			_direction = 1;
-		}
 		// Perform rotation
-		transform.Rotate(Vector3.up * Speed * 10f * _direction);
+		transform.Rotate(Vector3.up * Speed * 10f * Direction);
 	}
 	
 	/// <summary>
@@ -42,20 +30,8 @@ public class CubeBehaviours_Example : MonoBehaviour
 	/// </summary>
 	void MoveCube()
 	{
-		// If cube X position is above the threshold
-		if (transform.position.x > 5.0f)
-		{
-			// Switch direction
-			_direction = -1;
-		}
-		// If cube X position is below the threshold
-		else if (transform.position.x < -5.0f)
-		{
-			// Switch direction
-			_direction = 1;
-		}
 		// Perform movement 
-		transform.Translate(Vector3.left * Speed * _direction);
+		transform.Translate(Vector3.left * Speed * Direction);
 	}
 	
 	/// <summary>
